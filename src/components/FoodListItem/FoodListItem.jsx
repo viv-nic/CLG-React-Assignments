f08f114560cbe246e62c7d4f9701275f34ae5669
“viv-nic” <vivian1005@hotmail.com>
2022-12-05 11:26:54 +1100

    added styling to food gallery and changed main font color

--- src/components/FoodListItem/FoodListItem.jsx
+++ src/components/FoodListItem/FoodListItem.jsx
@@ -3,12 +3,17 @@ import styles from './FoodListItem.module.css';
 function FoodListItem(props) {
     return (
       <div className={styles.container}>
-        {!props.foodData.haveTried && <h1>MUST TRY</h1>}
+        {!props.foodData.haveTried && <h1 className={styles.mustTry}>MUST TRY</h1>}
+        
         <img src={props.foodData.image} height={280} alt={props.foodData.title} />
         <h3>
           {props.foodData.title} |{" "}
           {props.foodData.rating > 0 ? `${props.foodData.rating}/5` : `No Rating`}
+          
         </h3>
+        <h2>
+          {props.foodData.rating > 4.5 ? `Highly recommend!!!💖` : ``}
+        </h2>
         <p>{props.foodData.description}</p>
         
       </div>
