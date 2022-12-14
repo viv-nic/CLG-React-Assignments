f49ff14ec3ad29692e34e0dc9a7f016fb6df15cb
“viv-nic” <vivian1005@hotmail.com>
2022-12-14 22:17:02 +1100

    setting up initial testing with jest

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -4,9 +4,17 @@ import Button from "./index";
 import userEvent from "@testing-library/user-event";
 
 
-test('button should render', () => {
-    render(<Button />)
-    const btn = screen.getByRole('button');
-    expect(btn).toBeInTheDocument()
-});
+// test('button should render', () => {
+//     render(<Button />)
+//     const btn = screen.getByRole('button');
+//     expect(btn).toBeInTheDocument()
+// });
 
+//test if the button is clickable
+test('button should be clickable', () => {
+    const mockClickFn = jest.fn();
+    render(<Button onClick={mockClickFn} type='button'>Click Me</Button>)
+    const btn = screen.getByRole('button');
+    userEvent.click(btn);
+    expect(mockClickFn.mock.calls.length).toEqual(1);
+});
